0c07b91e15d6231d97381ebd7bd49741aa0f2db7
gauss314 <flodalila@gmail.com>
2021-04-11 19:35:39 -0300

    .

--- defi/defi_tools.py
+++ defi/defi_tools.py
@@ -24,7 +24,7 @@ def iloss(price_ratio):
         TYPE: impermanent loss decimal value, ie iloss:10%, returns 0.1
     """
     il = 2 * (price_ratio**0.5 / (1 + price_ratio)) - 1
-    return f"{il.:2%}"
+    return f"{il:.2%}"
 
 
 
