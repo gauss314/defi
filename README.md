d3b57b2b1df7baef739b4113ae1915bc9d8b29b4
gauss314 <flodalila@gmail.com>
2021-04-11 19:13:33 -0300

    .

--- README.md
+++ README.md
@@ -15,8 +15,14 @@ pip install defi
 ```python
 import defi
 
-# code
+compare(days=20, var_A=0, var_B=150, rw_pool_A=0.01, rw_pool_B=0.05, rw_pool_AB=0.2, fees_AB=0.01)
 ```
+{
+ 'buy_hold': '75.00%',
+ 'stake': '75.60%',
+ 'farm': '71.96%',
+ 'Mejor Estrategia': 'Stake'
+}
 
 
 ## About
--- defi/defi_tools.py
+++ defi/defi_tools.py
@@ -29,12 +29,12 @@ def iloss(price_ratio):
 
 
 
-def compare(dias, var_A=0, var_B=0, rw_pool_A=0, rw_pool_B=0, rw_pool_AB=0, fees_AB=0):
+def compare(days, var_A=0, var_B=0, rw_pool_A=0, rw_pool_B=0, rw_pool_AB=0, fees_AB=0):
     """Compare for 2 assets, buy&hold strategy with separate staking and farming by liquidity pool providing.
         Considering: impermanent loss, fees earned and farming/staking rewards
     
     Args:
-        dias (int): days for strategy
+        days (int): days for strategy
         var_A (float, optional): Percentual variation for A token. Ex 10 for 10%
         var_B (float, optional): Percentual variation for B token. Ex 10 for 10%
         rw_pool_A (float, optional): Percentual rewards per day for one asset pool (Token A)
@@ -52,8 +52,8 @@ def compare(dias, var_A=0, var_B=0, rw_pool_A=0, rw_pool_B=0, rw_pool_AB=0, fees
     x = (var_A/100 + 1) / (var_B/100 + 1)
     perdida_impermanente = 2 * (x**0.5 / (1 + x)) - 1
 
-    stake = buy_hold + 0.5 * dias * (rw_pool_A/100 + rw_pool_B/100)
-    farm = buy_hold * (1+perdida_impermanente) + dias * (rw_pool_AB/100 + fees_AB/100)
+    stake = buy_hold + 0.5 * days * (rw_pool_A/100 + rw_pool_B/100)
+    farm = buy_hold * (1+perdida_impermanente) + days * (rw_pool_AB/100 + fees_AB/100)
     mejor = 'Farm' if farm > stake else 'Stake'
     
     return {'buy_hold':f'{buy_hold:.2%}', 'stake':f'{stake:.2%}', 'farm':f'{farm:.2%}', 'Best': mejor}
