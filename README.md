74154a9cd5001e1fb97cf9ebbcb48ddb72215c0b
gauss314 <flodalila@gmail.com>
2021-04-17 04:50:16 -0300

    pancake swap

--- README.md
+++ README.md
@@ -142,7 +142,166 @@ Endpoints available, some examples:
 		# full history containing price, market cap and volume 
 
 
-full API wrapper soon
+### Examples
+
+#### Get price for coins at diferent currencies
+```python
+import defi.defi_tools as dft
+
+dft.geckoPrice("bitcoin,ethereum", "usd,eur,brl")
+```
+{'ethereum': {'usd': 2149.85, 'eur': 1807.58, 'brl': 12208.77},
+ 'bitcoin': {'usd': 60188, 'eur': 50606, 'brl': 341802}}
+
+
+
+
+#### Get main exchanges for a coin or token 
+```python
+import defi.defi_tools as dft
+
+df = dft.geckoMarkets("ethereum")
+print(df.info())
+# returns top 100 ethereum quotes by volume
+```
+
+Index: 100 entries, IDCM to FTX.US
+Data columns (total 9 columns):
+ #   Column       Non-Null Count  Dtype              
+---  ------       --------------  -----              
+ 0   base         100 non-null    object             
+ 1   target       100 non-null    object             
+ 2   last         100 non-null    float64            
+ 3   volume       100 non-null    float64            
+ 4   spread       100 non-null    float64            
+ 5   timestamp    100 non-null    datetime64[ns, UTC]
+ 6   volume_usd   100 non-null    float64            
+ 7   price_usd    100 non-null    float64            
+ 8   trust_score  100 non-null    object             
+dtypes: datetime64[ns, UTC](1), float64(5), object(3)
+memory usage: 7.8+ KB
+
+
+
+#### Get historical prices for a coin
+```python
+import defi.defi_tools as dft
+
+df = dft.geckoHistorical('cardano')
+print(df)
+```
+
+                        price   market_caps  total_volumes
+date                                                      
+2017-10-18 00:00:00  0.026845  6.960214e+08   2.351678e+06
+2017-10-19 00:00:00  0.026830  6.956220e+08   2.815156e+06
+2017-10-20 00:00:00  0.030300  7.855800e+08   8.883473e+06
+2017-10-21 00:00:00  0.028588  7.412021e+08   5.308857e+06
+2017-10-22 00:00:00  0.027796  7.206698e+08   2.901876e+06
+...                       ...           ...            ...
+2021-04-13 00:00:00  1.319790  4.223483e+10   5.005258e+09
+2021-04-14 00:00:00  1.422447  4.565529e+10   5.693373e+09
+2021-04-15 00:00:00  1.456105  4.676570e+10   8.920293e+09
+2021-04-16 00:00:00  1.478071  4.730118e+10   5.151595e+09
+2021-04-17 03:47:55  1.433489  4.595961e+10   5.152747e+09
+
+[1278 rows x 3 columns]
+
+
+
+
+## Pancake Swap
+
+### Get tokens prices in real time
+```python
+import defi.defi_tools as dft
+
+df = dft.pcsTokens()
+print(df)
+```
+                                                         name     symbol       price  price_BNB                 updated
+0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82  PancakeSwap Token       Cake     24.0636     0.0450 2021-04-17 04:29:08.332
+0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c        Wrapped BNB       WBNB    534.2575     1.0000 2021-04-17 04:29:08.332
+0x0F9E4D49f25de22c2202aF916B681FBB3790497B             Perlin        PRL      0.2091     0.0004 2021-04-17 04:29:08.332
+0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56         BUSD Token       BUSD      1.0000     0.0019 2021-04-17 04:29:08.332
+0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c         BTCB Token       BTCB  62166.5517   116.3604 2021-04-17 04:29:08.332
+...                                                       ...        ...         ...        ...                     ...
+0xB6802C06A441BA63624751C53C7c0708b75F06EC          FinalMoon  FINALMOON      0.0651     0.0001 2021-04-17 04:29:08.332
+0x2cF0DA1EB4165d73156CE1E32450e4A0E1c1791b        FairUnicorn       FUni      0.0000     0.0000 2021-04-17 04:29:08.332
+0x5CeD26185f82B07E1516d0B013c54CcBD252A4Ad            Peaches      PEACH      0.1130     0.0002 2021-04-17 04:29:08.332
+0x2bA64EFB7A4Ec8983E22A49c81fa216AC33f383A        Wrapped BGL       WBGL      0.1000     0.0002 2021-04-17 04:29:08.332
+0x019bE1796178516e060072004F267B59a49A0801     Pepper Finance       PEPR      0.1819     0.0003 2021-04-17 04:29:08.332
+
+[854 rows x 5 columns]
+
+
+
+### Get pairs, liquidity, and more
+```python
+import defi.defi_tools as dft
+
+pairs = dft.pcsPairs(as_df=False)
+print(pairs)
+```
+{'updated_at': 1618645355351,
+ 'data': {'0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82_0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c': 
+ 	{'pair_address': '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+	   'base_name': 'PancakeSwap Token',
+	   'base_symbol': 'Cake',
+	   'base_address': '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+	   'quote_name': 'Wrapped BNB',
+	   'quote_symbol': 'WBNB',
+	   'quote_address': '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+	   'price': '0.04503969270521829587',
+	   'base_volume': '5473068.824002232134035221',
+	   'quote_volume': '239997.1228321299572591638',
+	   'liquidity': '1076144814.0632013827775993748053',
+	   'liquidity_BNB': '2007551.221740467021401314'
+	},
+	.....
+}
+
+
+
+### Get token info
+```python
+import defi.defi_tools as dft
+dft.pcsTokenInfo('cake')
+```
+{'name': 'PancakeSwap Token',
+ 'symbol': 'Cake',
+ 'price': '24.03353223898417117634582253598019',
+ 'price_BNB': '0.04503467915973850237292527741402623'
+}
+
+
+### Get pair info
+```python
+import defi.defi_tools as dft
+dft.pcsPairInfo('cake','bnb')
+```
+{'pair_address': '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+ 'base_name': 'PancakeSwap Token',
+ 'base_symbol': 'Cake',
+ 'base_address': '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+ 'quote_name': 'Wrapped BNB',
+ 'quote_symbol': 'WBNB',
+ 'quote_address': '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+ 'price': '0.04503969270521829587',
+ 'base_volume': '5473068.824002232134035221',
+ 'quote_volume': '239997.1228321299572591638',
+ 'liquidity': '1076144814.0632013827775993748053',
+ 'liquidity_BNB': '2007551.221740467021401314'
+}
+
+
+### Simulate LP invest
+```python
+import defi.defi_tools as dft
+dft.value_f, iloss = iloss_simulate('cake','bnb', value=1000, base_pct_chg=50, quote_pct_chg=-25)
+```
+<img src="images/imp_loss_3d.png" width=600>
+
 
 
 
--- defi/defi_tools.py
+++ defi/defi_tools.py
@@ -3,6 +3,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime, requests
+from scipy import interpolate
+import matplotlib.cm as cm
+
+
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
@@ -61,12 +65,16 @@ def compare(days, var_A=0, var_B=0, rw_pool_A=0, rw_pool_B=0, rw_pool_AB=0, fees
     return {'buy_hold':f'{buy_hold:.2%}', 'stake':f'{stake:.2%}', 'farm':f'{farm:.2%}', 'Best': mejor}
 
 
-"""
-Llama API
 
-    Public API https://docs.llama.fi/api
 
-"""
+
+######################################################################
+##                                                                  ##
+##      Llama API                                                   ##
+##      Public API https://docs.llama.fi/api                        ##
+##                                                                  ##
+######################################################################
+
 
 def getProtocols():
     """Get list all DeFi protocols across all blockchains
@@ -121,12 +129,14 @@ def getChart():
 
 
 
-"""
-coinGecko API
+######################################################################
+##                                                                  ##
+##      CoinGecko API                                               ##
+##      Public API https://www.coingecko.com/es/api                 ##
+##                                                                  ##
+######################################################################
 
-    Public API https://www.coingecko.com/es/api#explore-api
 
-"""
 
 
 def geckoPrice(tokens, quote):
@@ -216,3 +226,258 @@ def geckoHistorical(ticker, vs_currency='usd'):
     df.set_index('date', inplace=True)
 
     return df
+
+
+
+######################################################################
+##                                                                  ##
+##       Pancake Swap API                                           ##
+##       API https://github.com/pancakeswap/pancake-info-api        ##
+##                                                                  ##
+######################################################################
+
+
+def toFloatPartial(df):
+    for i in df.columns:
+        try:
+            df[[i]] = df[[i]].astype(float)
+        except:
+            pass
+    return df
+
+        
+def pcsSummary(as_df = True ):
+
+    url = "https://api.pancakeswap.info/api/summary"
+    r = requests.get(url).json()
+    if as_df:
+        return pd.DataFrame.from_dict(r, orient='index')
+    else:
+        return r
+
+def pcsTokens(as_df = True):
+    """get all token listed in pancakeswap
+    
+    Args:
+        as_df (bool, optional): if True (default), return is a dataframe, else is a dictionary
+    
+    Returns:
+        DataFrame with next columns: name  symbol  price   price_BNB   updated
+
+    """
+    # ultimo precio y volumen de base/quote de todos los pares
+
+    url = "https://api.pancakeswap.info/api/tokens"
+    r = requests.get(url).json()
+    data = r.get('data', None)
+    upd = r.get('updated_at')/1000
+    upd_dt = datetime.datetime.fromtimestamp(upd)
+    
+    if as_df:
+        df = pd.DataFrame.from_dict(data, orient='index')
+        df = toFloatPartial(df) 
+        df['updated'] = upd_dt
+        return df
+    else:
+        return r
+
+
+def pcsPairs(as_df = True):
+    """get top 1000 pancakeswap pairs LP order by reserves
+    
+    Args:
+        as_df (bool, optional): if True (default), return is a dataframe, else is a dictionary
+    
+    Returns:
+        DataFrame with next columns: 'pair_address', 'base_name', 'base_symbol', 'base_address',
+       'quote_name', 'quote_symbol', 'quote_address', 'price', 'base_volume',
+       'quote_volume', 'liquidity', 'liquidity_BNB', 'updated'
+    """
+
+    url = "https://api.pancakeswap.info/api/pairs"
+    r = requests.get(url).json()
+    data = r.get('data', None)
+    upd = r.get('updated_at')/1000
+    upd_dt = datetime.datetime.fromtimestamp(upd)
+    
+    if as_df:
+        df = pd.DataFrame.from_dict(data, orient='index')
+        df = toFloatPartial(df) 
+        df['updated'] = upd_dt
+        return df
+    else:
+        return r
+
+def pcsTokenInfo(search):
+    """get info from a token
+    
+    Args:
+        search (string): Token symbol or contract address
+    
+    Returns:
+        Dict: 
+        {
+         'name': 'Wrapped BNB',
+         'symbol': 'WBNB',
+         'price': '524.5429',
+         'price_BNB': '1'
+         }
+    """
+    search = 'WBNB' if search.upper() == 'BNB' else search   
+    url = "https://api.pancakeswap.info/api/tokens"
+    r = requests.get(url).json()
+    data = r.get('data', None)
+    res = f"Not found: {search}"
+    for contract, values in data.items():
+        if search.upper() == values['symbol'].upper() or search.upper()==contract.upper():
+            res = data[contract]
+            break
+
+    return res
+
+
+
+def pcsPairInfo(base, quote):
+    """get info from a token pair LP
+    
+    Args:
+        base (string): Base LP token, ie "CAKE"
+        quote (string): Quote LP token, ie "BNB"
+        its the same if you call pcsPAirInfo('cake', 'bnb') or pcsPAirInfo('bnb', 'cake') 
+    Returns:
+        Dict: {
+                 'pair_address': '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
+                 'base_name': 'PancakeSwap Token',
+                 'base_symbol': 'Cake',
+                 'base_address': '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+                 'quote_name': 'Wrapped BNB',
+                 'quote_symbol': 'WBNB',
+                 'quote_address': '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+                 'price': '0.04311198194009326668',
+                 'base_volume': '22248744.85',
+                 'quote_volume': '934856.36',
+                 'liquidity': '982769040.63',
+                 'liquidity_BNB': '1878155.84'
+                }
+                * price is actually a ratio between base/quote tokens
+    """
+    url = "https://api.pancakeswap.info/api/pairs"
+    r = requests.get(url).json()
+    data = r.get('data', None)
+    res = f"Not found: {base}-{quote}"
+    base = 'WBNB' if base.upper() == 'BNB' else base
+    quote = 'WBNB' if quote.upper() == 'BNB' else quote
+    
+    for contract, values in data.items():
+        base_ = base.upper() == values['base_symbol'].upper()
+        quote_ = quote.upper() == values['quote_symbol'].upper()
+        base_cross = base.upper() == values['quote_symbol'].upper()
+        quote_cross = quote.upper() == values['base_symbol'].upper()
+
+        if  (base_ and quote_) or  (base_cross and quote_cross):
+            res = data[contract]
+            break
+            
+    return res
+
+
+from scipy import interpolate
+import matplotlib.cm as cm
+
+def iloss_simulate(base_token, quote_token, value=100, base_pct_chg=0, quote_pct_chg=0):
+    """Calculate simulated impermanent loss from an initial value invested, get real time prices from pancakeswap API
+        This method create a 3D interpolated surface for impermanent loss and initial/final value invested
+
+    Args:
+        base_token (string): Pair first token, ie CAKE
+        quote_token (string): Pais second token, ie BNB
+        value (int, optional): Value investen in LP default=100
+        base_pct_chg (int, optional): value assming will change first token of LP pair, ie 10 (for +10% change)
+        quote_pct_chg (int, optional): value assming will change first token of LP pair, ie -30 (for -30% change)
+    
+    Returns:
+        tuple (value_f, iloss): final value of value invested, and decimal impermanent loss
+    """
+    base_token = 'WBNB' if base_token.upper() == 'BNB' else base_token
+    quote_token = 'WBNB' if quote_token.upper() == 'BNB' else quote_token
+    
+    # get real time prices
+    tokens = pcsTokens()
+    px_base = float(tokens.loc[tokens.symbol.str.upper()==base_token.upper()].price)
+    px_quote = float(tokens.loc[tokens.symbol.str.upper()==quote_token.upper()].price)
+
+    # Prepare grid
+    q_base, q_quote = (value/2)/px_base,  (value/2)/px_quote
+    px_base, px_quote, q_base, q_quote
+    pxs_base = [px_base*i/100 for i in range(1,301)]
+    pxs_quote = [px_quote*i/100 for i in range(1,301)]
+    rows = []
+    for px_b in pxs_base:
+        for px_q in pxs_quote:
+            ratio = (px_b / px_base) / (px_q / px_quote)
+            iloss = 2 * (ratio**0.5 / (1 + ratio)) - 1
+
+            row = {'px_base':px_b, 'px_quote':px_q, 
+                   'ratio':(px_b / px_base) / (px_q / px_quote),
+                   'impremante_loss':iloss}
+            rows.append(row)
+    df = pd.DataFrame(rows)
+    df_ok = df.loc[:,['px_base','px_quote','impremante_loss']]
+    df_ok = df_ok.replace('NaN',np.nan).dropna()
+
+    if all(isinstance(i, (int, float)) for i in (value, base_pct_chg, quote_pct_chg)):
+        px_base_f = px_base * (1+base_pct_chg/100)
+        px_quote_f = px_quote * (1+quote_pct_chg/100)
+        ratio = (px_base_f / px_base) / ( px_quote_f / px_quote)
+        iloss = 2 * (ratio**0.5 / (1 + ratio)) - 1
+        value_f = (px_base_f*q_base + px_quote_f * q_quote) * (iloss+1)
+    else:
+        px_base_f, px_quote_f = px_base, px_quote
+        iloss = 0
+        value_f = None
+        print('must input numerical amount and pct change for base and quote to calculations of final value')
+        
+
+    # Ploting surface
+    fig = plt.figure(figsize=(8,8))
+    x1 = np.linspace(df_ok['px_base'].min(), df_ok['px_base'].max(), len(df_ok['px_base'].unique()))
+    y1 = np.linspace(df_ok['px_quote'].min(), df_ok['px_quote'].max(), len(df_ok['px_quote'].unique()))
+    x2, y2 = np.meshgrid(x1, y1)
+    Z = interpolate.griddata((df_ok['px_base'], df_ok['px_quote']), df_ok['impremante_loss'], (x2, y2))
+    Z[np.isnan(Z)] = df_ok.impremante_loss.mean()
+    ax = plt.axes(projection='3d', alpha=0.2)
+    ax.plot_wireframe(x2, y2, Z, color='tab:blue', lw=1, cmap='viridis', alpha=0.6) 
+        
+    # Start values ploting
+    xmax = df_ok.px_base.max() 
+    ymax = df_ok.px_quote.max()
+    ax.plot([px_base, px_base], [0,px_quote], [-1,-1], ls='--', c='k', lw=1)
+    ax.plot([px_base, px_base], [px_quote,px_quote], [0,-1], ls='--', c='k', lw=1)
+    ax.plot([px_base, 0], [px_quote, px_quote], [-1,-1], ls='--', c='k', lw=1)
+
+    # End values ploting
+    ax.plot([px_base_f, px_base_f], [0,px_quote_f], [-1,-1], ls='--', c='gray', lw=1)
+    ax.plot([px_base_f, px_base_f], [px_quote_f,px_quote_f], [iloss,-1], ls='--', c='gray', lw=1)
+    ax.plot([px_base_f, 0], [px_quote_f, px_quote_f], [-1,-1], ls='--', c='gray', lw=1)
+    ax.plot([px_base_f, px_base_f], [px_quote_f,ymax], [iloss,iloss], ls='--', c='gray', lw=1)
+    ax.plot([px_base_f, 0], [ymax,ymax], [iloss,iloss], ls='--', c='gray', lw=1)
+    
+    # Plot settings
+    # Colorbar only for plot_surface() method instead plot_wireframe()
+    # m = cm.ScalarMappable(cmap=cm.viridis) 
+    # m.set_array(df_ok['impremante_loss'])
+    # plt.colorbar(m, fraction=0.02, pad=0.1)
+    x, y, z = (px_base, px_quote,.05)
+    p = ax.scatter(x, y, z, c='k', marker='v', s=300)
+    ax.set_title('Impermanent Loss 3D Surface', y=0.95)
+    ax.set_xlabel(f'Price {base_token}')
+    ax.set_ylabel(f'Price {quote_token}')
+    ax.set_zlabel('Impremante loss')
+    ax.view_init(elev=25, azim=240) # start view angle
+    
+    print (f"\nStart value USD {value:.0f}, {base} USD {px_base:.2f}, {quote} USD {px_quote:.2f}")    
+    print(f"\nResults assuming {base.upper()} {base_pct_chg}%, and {quote.upper()} {quote_pct_chg}%")
+    print (f"End value estimate USD {value_f:.0f}, iloss: {iloss:.2%}")
+    plt.show()
+
+    return value_f , iloss
